78871528dc25a7b3f89714c0eb45dc8065063885
shanto096 <shanto096@gmail.com>
2025-04-18 16:16:57 +0800

    add

--- image-to-text-test/test.js
+++
@@ -1,9 +0,0 @@
-import { imageToText } from '@shanto/image-to-text-convert';
-import fs from 'fs';
-const imageBuffer = fs.readFileSync('Minimalist Professional CV Resume.png');
-
-imageToText(imageBuffer).then(text => {
-    console.log('📄 Extracted Text:\n', text);
-}).catch(err => {
-    console.error('❌ Error:', err);
-});
